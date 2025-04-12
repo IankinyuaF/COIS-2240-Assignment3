3436d253e33d81e5fbaf901033d3ea49e0fe6602
IanKinyuaF <mwangiian979@gmail.com>
2025-04-11 22:18:06 -0400

    Refactored Vehicle constructor to use capitalize(5)

--- RentalProject/src/Vehicle.java
+++ RentalProject/src/Vehicle.java
@@ -1,52 +1,53 @@
 public abstract class Vehicle {
+    public enum VehicleStatus { AVAILABLE, RENTED }
+
     private String licensePlate;
     private String make;
     private String model;
     private int year;
     private VehicleStatus status;
 
-    public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
-
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+        this.make = capitalize(make);
+        this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
-        this.licensePlate = null;
     }
 
-    public Vehicle() {
-        this(null, null, 0);
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate.toUpperCase();
     }
 
-    public void setLicensePlate(String plate) {
-        this.licensePlate = plate == null ? null : plate.toUpperCase();
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public VehicleStatus getStatus() {
+        return status;
     }
 
     public void setStatus(VehicleStatus status) {
-    	this.status = status;
+        this.status = status;
     }
 
-    public String getLicensePlate() { return licensePlate; }
+    public abstract String getInfo();
 
-    public String getMake() { return make; }
-
-    public String getModel() { return model;}
-
-    public int getYear() { return year; }
-
-    public VehicleStatus getStatus() { return status; }
-
-    public String getInfo() {
-        return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
+    //private helper method
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) return "";
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
-
 }
+
